0874ca33e544712ad5c64edb636225575d762159
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-04-23 13:08:41 -0500

    updating formatand comments

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,6 +1,7 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
 let basket=['cheese','bread','eggs','milk'];
 const maxItems=5;
 
@@ -8,24 +9,28 @@ const maxItems=5;
 function addItem(){
 let added = basket.unshift('apple');// I think this is the intended way for completeing the function addItem
 console.log(`The basket now has the following items: ${basket}`);
-//unshift. While working on this I had issues because I was using shift instead of unshift
 }
+//unshift. While working on this I had issues because I was using shift instead of unshift
+
+
 //- take an input parameter for a string `item`
 // add the new item to the global array `basket`. 
 // return `true` indicating the item was added
 
-function listItems(){
-//for loop. While first going over the assignment I was creating the functions and just writing down what I thought was the correct way to do each function. So thats why these are here
+
+function listItems(){ // for loop. While first going over the assignment I was creating the functions and just writing down what I thought was the correct way to do each function. So thats why these are here
 for (i=0; i<basket.length;i++){
     console.log(basket[i]);
+    }
 }
-}// loop over the items in the `basket` array
+// loop over the items in the `basket` array
 //console.log each individual item on a new line
 
 function empty(){
     basket.splice(0,basket.length);
     console.log(basket,'no items in the basket');
-}// clear items from basket with splice. the splice made this really easy
+}
+// clear items from basket with splice. the splice made this really easy
 
 function isFull(){
     if (basket.length < maxItems){
@@ -36,6 +41,7 @@ function isFull(){
     }
 }
 //if and else statement using .length
+
 function removeItem(){
     console.log(basket.pop())
 }
